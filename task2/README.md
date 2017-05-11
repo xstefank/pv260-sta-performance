a843094d5c6094e0ea7a709efabfa5d10da7a0bd
xstefank <xstefank122@gmail.com>
2017-05-12 00:26:27 +0200

    task2 - add postgres database

---
+++ task2/README.md
@@ -0,0 +1,9 @@
+
+# Task 2 - Database performace comparisons
+
+## PostgreSQL setup
+1. download postgres binaries from https://www.enterprisedb.com/products-services-training/pgbindownload
+1. run the postgres-startup.sh
+    * **Note that when running for the first time you need to uncomment the initialization line**
+
+
---
+++ task2/postgres-startup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Setting environment variables..."
+
+PGSQL_HOME=$(pwd)/pgsql
+export PATH=$PATH:$PGSQL_HOME/bin
+export PGDATA=$PGSQL_HOME/data
+export PGDATABASE=postgres
+export PGUSER=postgres
+export PGPORT=5439
+export PGLOCALEDIR=$PGSQL_HOME/share/locale
+
+# init DB - uncomment this if you are running this script for the first time
+# echo "Initializing DB..." && $PGSQL_HOME/bin/initdb -U postgres -A trust
+
+echo "Starting ctl..."
+$PGSQL_HOME/bin/pg_ctl -D $PGSQL_HOME/data -l logfile start
+
+echo "Press enter to exit"
+read
+
+$PGSQL_HOME/bin/pg_ctl -D $PGSQL_HOME/data stop
+
+
